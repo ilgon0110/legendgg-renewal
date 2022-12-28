31cf4cca0a5f65d3e3c895736d049e9d213f7fcc
ilgon0110 <moses011010@gmail.com>
2022-12-28 18:25:30 +0900

    style : stat page styling

--- pages/stats/index.tsx
+++ pages/stats/index.tsx
@@ -1,3 +1,188 @@
-export default function Stats() {
-  return <div>stats page</div>;
+import dynamic from 'next/dynamic';
+import {
+  Container,
+  Item,
+  Profile,
+  Img,
+  Logo,
+  LogoName,
+  WinBar,
+  WinRateBar,
+  WinRate,
+  WinRateRank,
+  Year,
+  Season,
+  ChartBox,
+  YearSelect,
+  SeasonSelect,
+  StatBox,
+  PlayerSaying,
+  CareerText,
+  CareerBox,
+} from '../../styles/stats';
+
+export default function StatOfPlayer() {
+  const chartData = [
+    {
+      x: 'KDA',
+      y: 4.3 * 10,
+    },
+    {
+      x: 'KP',
+      y: 73.4,
+    },
+    {
+      x: 'DTH',
+      y: 21,
+    },
+    {
+      x: 'GOLD',
+      y: 29.1,
+    },
+    {
+      x: 'WIN',
+      y: 70,
+    },
+  ];
+  const ApexChart = dynamic(() => import('react-apexcharts'), {
+    ssr: false,
+  });
+
+  return (
+    <Container>
+      <Item>
+        <Profile>
+          <Img src={`/imgs/profiles/faker.png`} alt={'profile'}></Img>
+          <Logo src={`/imgs/team_logos/T1.png`} alt={'Logo'}></Logo>
+          <LogoName>{'T1'}</LogoName>
+          <WinBar>
+            <WinRateBar winrate={70}></WinRateBar>
+          </WinBar>
+          <WinRate>
+            {'70%'}
+            <WinRateRank>{'2nd'}</WinRateRank>
+          </WinRate>
+          <Year>
+            {2013}
+            <Season>{'Spring'}</Season>
+          </Year>
+          <ChartBox>
+            <ApexChart
+              type="radar"
+              width="100%"
+              height="100%"
+              series={[
+                {
+                  name: `${2013} ${'Spring'}`,
+                  data: chartData,
+                },
+              ]}
+              options={{
+                chart: {
+                  toolbar: {
+                    show: false,
+                  },
+                },
+                xaxis: {
+                  categories: ['KDA', 'KP%', 'DTH%', 'GOLD', 'WIN%'],
+                },
+                yaxis: {
+                  show: false,
+                  tickAmount: 3,
+                  min: -100,
+                  max: 100,
+                  labels: {
+                    formatter: (a: any) => a.toFixed(1),
+                  },
+                },
+                dataLabels: {
+                  enabled: true,
+                  formatter: (element: number, { dataPointIndex }) =>
+                    `${
+                      dataPointIndex === 0
+                        ? element / 10
+                        : dataPointIndex === 2
+                        ? element / 10
+                        : dataPointIndex === 3
+                        ? element * 10
+                        : element
+                    }`,
+                },
+                tooltip: {
+                  enabled: false,
+                },
+                responsive: [
+                  {
+                    breakpoint: 578,
+                    options: { dataLabels: { enabled: false } },
+                  },
+                ],
+              }}
+            ></ApexChart>
+          </ChartBox>
+        </Profile>
+      </Item>
+      <Item>
+        <YearSelect id="year">
+          {['2013', '2014', '2015'].map((option) => (
+            <option value={option} key={option}>
+              {option}
+            </option>
+          ))}
+        </YearSelect>
+        <SeasonSelect opacity={0.8}>Spring</SeasonSelect>
+        <SeasonSelect opacity={0.8}>Summer</SeasonSelect>
+        <SeasonSelect opacity={0.8}>Worlds</SeasonSelect>
+        {
+          <>
+            <StatBox>
+              <div>
+                <h1>{4.3}</h1>
+                <h1>{'3rd'}</h1>
+                <h1>KDA</h1>
+              </div>
+              <div>
+                <h1>{73.4}</h1>
+                {<h1>{'2nd'}</h1>}
+                <h1>{'KP%'}</h1>
+              </div>
+              <div>
+                <h1>{21}</h1>
+                {<h1>{'8th'}</h1>}
+                <h1>{'DTH%'}</h1>
+              </div>
+              <div>
+                <h1>{'29.1%'}</h1>
+
+                {<h1>{'1st'}</h1>}
+                <h1>{'GOLD%'}</h1>
+              </div>
+              <div>
+                <h1>{'70%'}</h1>
+                {<h1>{'2nd'}</h1>}
+                <h1>{'WINRATE%'}</h1>
+              </div>
+            </StatBox>
+            <PlayerSaying>역대 최고의 리그 오브 레전드 프로게이머</PlayerSaying>
+            <CareerText>커리어</CareerText>
+            <CareerBox>
+              <div>
+                <h1>{'1st'}</h1>
+                <h1>정규시즌</h1>
+              </div>
+              <div>
+                <h1>{'3rd'}</h1>
+                <h1>플레이오프</h1>
+              </div>
+              <div>
+                <h1>{'650'}</h1>
+                <h1>{'4th'}</h1>
+                <h1>POG포인트</h1>
+              </div>
+            </CareerBox>
+          </>
+        }
+      </Item>
+    </Container>
+  );
 }
---
+++ styles/stats/index.ts
@@ -0,0 +1,366 @@
+import styled from 'styled-components';
+import { media } from '../../styles/theme';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  column-gap: 30px;
+  padding-top: 56px;
+  grid-template-columns: repeat(12, 1fr);
+  ${() => media.tablet} {
+    padding: 56px 24px;
+  }
+  ${() => media.mobile} {
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: 1fr 5fr;
+    row-gap: 12px;
+  }
+`;
+export const Item = styled.div`
+  position: relative;
+  &:nth-child(1) {
+    grid-column: 1/4;
+    border: 1px solid blue;
+    ${() => media.mobile} {
+      grid-column: span 4;
+      padding: 0px 16px;
+      height: 30%;
+    }
+  }
+  &:last-child {
+    grid-column: 4/13;
+    border: 1px solid red;
+    ${() => media.mobile} {
+      grid-column: span 4;
+      padding: 0px 16px;
+    }
+  }
+`;
+export const Profile = styled.div`
+  position: relative;
+  width: 100%;
+  height: 97%;
+  margin-top: 8px;
+  background: transparent;
+  border-radius: 2px;
+  box-shadow: 0px 0px 6px #00000029;
+  border: 1px solid green;
+`;
+export const Img = styled.img`
+  position: relative;
+  display: block;
+  width: 170px;
+  height: 227px;
+  top: 8px;
+  margin: 0 auto;
+  ${() => media.tablet} {
+    width: 115px;
+    height: 155px;
+  }
+  ${() => media.mobile} {
+    width: 68px;
+    height: 91px;
+    margin: 0px;
+    left: 10%;
+  }
+`;
+export const Logo = styled.img`
+  position: relative;
+  display: block;
+  width: 32px;
+  height: 32px;
+  margin: 0 auto;
+  top: 12px;
+  ${() => media.tablet} {
+    width: 20px;
+    height: 20px;
+  }
+  ${() => media.mobile} {
+    margin-top: -72px;
+  }
+`;
+export const LogoName = styled.h1`
+  position: relative;
+  text-align: center;
+  color: white;
+  font-size: 12px;
+  font-weight: 300;
+  opacity: 0.7;
+  top: 12px;
+  ${() => media.tablet} {
+    font-size: 8px;
+  }
+`;
+export const WinBar = styled.div`
+  position: relative;
+  margin: 0 auto;
+  width: 80%;
+  height: 24px;
+  background: white;
+  top: 32px;
+  ${() => media.tablet} {
+    height: 16px;
+  }
+  ${() => media.mobile} {
+    top: 20px;
+    width: 20%;
+    height: 10px;
+  }
+`;
+export const WinRateBar = styled.div<{ winrate: number | undefined }>`
+  position: relative;
+  width: ${(props) => props.winrate}%;
+  height: 24px;
+  background: #7081f3;
+  ${() => media.tablet} {
+    height: 16px;
+  }
+  ${() => media.mobile} {
+    height: 10px;
+  }
+`;
+export const WinRate = styled.h1`
+  position: relative;
+  text-align: center;
+  top: 36px;
+  font-size: 16px;
+  font-weight: normal;
+  color: white;
+  ${() => media.mobile} {
+    top: 24px;
+    font-size: 8px;
+  }
+`;
+export const WinRateRank = styled.span`
+  padding-left: 4px;
+  font-size: 12px;
+  font-weight: 300;
+  opacity: 0.7;
+  ${() => media.mobile} {
+    font-size: 8px;
+  }
+`;
+export const Year = styled.h1`
+  position: relative;
+  text-align: center;
+  top: 52px;
+  font-size: 18px;
+  font-weight: normal;
+  color: white;
+  ${() => media.mobile} {
+    position: absolute;
+    margin-top: -48px;
+    top: 52px;
+    right: 10%;
+    font-size: 12px;
+  }
+`;
+export const Season = styled.span`
+  padding-left: 8px;
+  font-size: 16px;
+  ${() => media.mobile} {
+    top: 0px;
+    font-size: 10px;
+  }
+`;
+export const ChartBox = styled.div`
+  position: relative;
+  margin: 0 auto;
+  width: 100%;
+  height: 228px;
+  top: 60px;
+  display: flex;
+  justify-content: center;
+  box-sizing: border-box;
+  ${() => media.tablet} {
+    height: 193px;
+  }
+  ${() => media.mobile} {
+    position: absolute;
+    width: 130px;
+    height: 130px;
+    top: 0px;
+    right: 3%;
+  }
+`;
+export const YearSelect = styled.select`
+  margin-top: 8px;
+  color: white;
+  border: none;
+  background: transparent;
+  font-size: 24px;
+  &:focus {
+    border: none;
+  }
+  &:active {
+    border: none;
+  }
+  option {
+    display: block;
+    background-color: rgba(0, 0, 0, 0);
+    color: black;
+  }
+  ${() => media.tablet} {
+    font-size: 20px;
+  }
+  ${() => media.mobile} {
+    font-size: 16px;
+    margin-left: 16px;
+  }
+`;
+export const SeasonSelect = styled.span<{ opacity: number }>`
+  font-size: 20px;
+  color: white;
+  padding: 32px;
+  font-weight: normal;
+  opacity: ${(props) => props.opacity};
+  &:hover {
+    cursor: pointer;
+  }
+  ${() => media.tablet} {
+    font-size: 16px;
+    padding: 20px;
+  }
+  ${() => media.mobile} {
+    font-size: 12px;
+    padding: 14px;
+  }
+`;
+export const StatBox = styled.div`
+  position: relative;
+  width: 100%;
+  height: 182px;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: 182px;
+  background: #0a2742;
+  border-radius: 12px;
+  top: 16px;
+  color: white;
+  align-items: center;
+  ${() => media.mobile} {
+    height: 100px;
+    grid-template-rows: 100px;
+  }
+  div {
+    position: relative;
+    text-align: center;
+    h1:first-child {
+      font-size: 32px;
+      color: ${(props) => props.theme.color.mint};
+      font-weight: 600;
+      padding-bottom: 4px;
+      ${() => media.tablet} {
+        font-size: 22px;
+      }
+      ${() => media.mobile} {
+        font-size: 16px;
+      }
+    }
+    h1:nth-child(2) {
+      font-size: 20px;
+      font-weight: 300;
+      opacity: 0.8;
+      padding-bottom: 20px;
+      ${() => media.tablet} {
+        font-size: 16px;
+      }
+      ${() => media.mobile} {
+        font-size: 10px;
+      }
+    }
+    h1:last-child {
+      font-size: 20px;
+      ${() => media.tablet} {
+        font-size: 16px;
+      }
+      ${() => media.mobile} {
+        font-size: 10px;
+      }
+    }
+    &:first-child {
+      grid-column: span 1;
+    }
+    &:last-child {
+      grid-column: span 1;
+    }
+  }
+`;
+export const CareerText = styled.h1`
+  position: relative;
+  color: white;
+  top: 120px;
+  font-size: 20px;
+  ${() => media.mobile} {
+    font-size: 12px;
+  }
+`;
+export const CareerBox = styled.div`
+  position: relative;
+  display: grid;
+  width: 100%;
+  height: 212px;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 212px;
+  column-gap: 30px;
+  color: white;
+  text-align: center;
+  ${() => media.mobile} {
+    column-gap: 12px;
+    height: 120px;
+    grid-template-rows: 120px;
+  }
+  div {
+    position: relative;
+    top: 135px;
+    border-radius: 12px;
+    font-size: 20px;
+    ${() => media.tablet} {
+      font-size: 14px;
+    }
+
+    h1:first-child {
+      margin-top: 60px;
+      font-size: 32px;
+      color: ${(props) => props.theme.color.mint};
+      font-weight: 600;
+      padding-bottom: 4px;
+      ${() => media.tablet} {
+        font-size: 22px;
+      }
+      ${() => media.mobile} {
+        margin-top: 24px;
+        font-size: 18px;
+      }
+    }
+    h1:last-child {
+      position: absolute;
+      top: 80%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      ${() => media.tablet} {
+        font-size: 22px;
+      }
+      ${() => media.mobile} {
+        font-size: 10px;
+      }
+    }
+    &:first-child {
+      grid-column: span 1;
+    }
+    &:last-child {
+      grid-column: span 1;
+    }
+    background: #0a2742;
+  }
+`;
+export const PlayerSaying = styled.h1`
+  position: relative;
+  color: white;
+  text-align: center;
+  top: 50px;
+  font-size: 16px;
+  opacity: 0.8;
+  font-style: italic;
+`;

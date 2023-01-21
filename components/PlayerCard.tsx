b1aaffceff5126d679c181dcbbbd91a046348da8
ilgon0110 <moses011010@gmail.com>
2023-01-21 15:27:47 +0900

    feat : PlayerCard Component 구현
    player page에서 검색이 정상적으로 이뤄졌을시 render되는 Component
    Container-Component 구조로 Redux의존성을 분리하여 재사용 가능하게 함

---
+++ components/PlayerCard.tsx
@@ -0,0 +1,205 @@
+import Image from 'next/image';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { playersActions } from '../store';
+import useSWR from 'swr';
+import { IRootState } from '../store';
+import { useEffect, useState } from 'react';
+import { IplayerData } from '../pages/stats/[id]';
+import { setRank } from '../utilities';
+import { media } from '../styles/theme';
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: fit-content;
+  border: 1px solid green;
+`;
+const Profile = styled.div`
+  position: relative;
+  margin: 0 auto;
+  width: 170px;
+  height: 227px;
+  ${() => media.mobile} {
+    width: 51px;
+    height: 69px;
+  }
+`;
+const Logo = styled.div`
+  position: relative;
+  margin: 0 auto;
+  width: 32px;
+  height: 32px;
+  ${() => media.mobile} {
+    width: 16px;
+    height: 16px;
+  }
+`;
+const LogoName = styled.h1`
+  position: relative;
+  text-align: center;
+  color: white;
+  font-size: 12px;
+  font-weight: 300;
+  opacity: 0.7;
+  margin-bottom: 8px;
+  ${() => media.mobile} {
+    font-size: 8px;
+  }
+`;
+const DarkBlue = styled.div`
+  position: relative;
+  background: #0a2742;
+  border-radius: 12px;
+  width: 100%;
+  height: 222px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-content: space-around;
+  color: white;
+  margin-bottom: 4px;
+  ${() => media.mobile} {
+    height: 152px;
+  }
+`;
+const DarkBlueItem = styled.div`
+  position: relative;
+  width: 33%;
+  text-align: center;
+  grid-column: span 1;
+`;
+const Score = styled.h1`
+  margin-bottom: 2px;
+  color: #008ffb;
+  font-weight: 600;
+`;
+const Nth = styled.h1`
+  font-size: 12px;
+  opacity: 0.7;
+  margin-bottom: 10px;
+  font-weight: lighter;
+`;
+const Title = styled.h1`
+  font-size: 12px;
+`;
+const Years = styled.span`
+  position: relative;
+  color: white;
+  padding-right: 12px;
+`;
+const Seasons = styled.span`
+  position: relative;
+  color: white;
+`;
+
+const PlayerCard = ({
+  name,
+  year,
+  season,
+  data,
+}: {
+  name: string;
+  year: string;
+  season: string;
+  data: IplayerData | undefined;
+}) => {
+  return (
+    <Wrapper>
+      <>
+        <Profile>
+          <Image
+            src={`/imgs/profiles/${name}.png`}
+            alt={'profile'}
+            fill
+            sizes="100%"
+            quality={100}
+            priority
+          />
+        </Profile>
+        <Logo>
+          <Image
+            src={`/imgs/team_logos/${data?.team}.png`}
+            alt={'profile'}
+            fill
+            sizes="100%"
+            quality={100}
+            priority
+          />
+        </Logo>
+        <LogoName>{data?.team}</LogoName>
+        {Number(year) >= 2015 ? (
+          <DarkBlue>
+            <DarkBlueItem>
+              <Score>{data?.winRate}</Score>
+              <Nth>{setRank(data?.winRateRank)}</Nth>
+              <Title>{'WIN'}</Title>
+            </DarkBlueItem>
+            <DarkBlueItem>
+              <Score>{data?.kda}</Score>
+              <Nth>{setRank(data?.kdaRank)}</Nth>
+              <Title>{'KDA'}</Title>
+            </DarkBlueItem>
+            <DarkBlueItem>
+              <Score>{data?.gd10}</Score>
+              <Nth>{setRank(data?.gd10Rank)}</Nth>
+              <Title>{'GD@10'}</Title>
+            </DarkBlueItem>
+            <DarkBlueItem>
+              <Score>{data?.cs10}</Score>
+              <Nth>{setRank(data?.cs10Rank)}</Nth>
+              <Title>{'CS@10'}</Title>
+            </DarkBlueItem>
+            <DarkBlueItem>
+              <Score>{`${data?.dpm}`}</Score>
+              <Nth>{setRank(data?.dpmRank)}</Nth>
+              <Title>{'DPM'}</Title>
+            </DarkBlueItem>
+            <DarkBlueItem>
+              <Score>{`${data?.dmg}%`}</Score>
+              <Nth>{setRank(data?.dmgRank)}</Nth>
+              <Title>{'DMG'}</Title>
+            </DarkBlueItem>
+          </DarkBlue>
+        ) : (
+          <DarkBlue>
+            <DarkBlueItem>
+              <Score>{data?.winRate}</Score>
+              <Nth>{setRank(data?.winRateRank)}</Nth>
+              <Title>{'WIN'}</Title>
+            </DarkBlueItem>
+            <DarkBlueItem>
+              <Score>{data?.kda}</Score>
+              <Nth>{setRank(data?.kdaRank)}</Nth>
+              <Title>{'KDA'}</Title>
+            </DarkBlueItem>
+            <DarkBlueItem>
+              <Score>{data?.kp}</Score>
+              <Nth>{setRank(data?.kpRank)}</Nth>
+              <Title>{'KP'}</Title>
+            </DarkBlueItem>
+            <DarkBlueItem>
+              <Score>{data?.dth}</Score>
+              <Nth>{setRank(data?.dthRank)}</Nth>
+              <Title>{'DTH'}</Title>
+            </DarkBlueItem>
+            <DarkBlueItem>
+              <Score>{`${data?.gold}%`}</Score>
+              <Nth>{setRank(data?.goldRank)}</Nth>
+              <Title>{'GOLD'}</Title>
+            </DarkBlueItem>
+            <DarkBlueItem>
+              <Score>{`${data?.winRate}%`}</Score>
+              <Nth>{setRank(data?.winRateRank)}</Nth>
+              <Title>{'WINRATE'}</Title>
+            </DarkBlueItem>
+          </DarkBlue>
+        )}
+        <Years>{year}</Years>
+        <Seasons>{season}</Seasons>
+      </>
+    </Wrapper>
+  );
+};
+
+export default PlayerCard;
---
+++ controllers/PlayerCard1Container.tsx
@@ -0,0 +1,84 @@
+import Image from 'next/image';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { chartActions, playersActions } from '../store';
+import useSWR from 'swr';
+import { IRootState } from '../store';
+import { useEffect, useState } from 'react';
+import { IplayerData } from '../pages/stats/[id]';
+import { setRank } from '../utilities';
+import PlayerCard from '../components/PlayerCard';
+
+const Button = styled.button`
+  display: block;
+  margin: 0 auto;
+  margin-bottom: 12px;
+  border-radius: 50%;
+  border: none;
+  box-shadow: 0px 0px 6px #00000029;
+
+  svg {
+    width: 25px;
+    height: 25px;
+  }
+`;
+
+const PlayerCard1Container = () => {
+  const dispatch = useDispatch();
+  const { playerOneName, playerOneYear, playerOneSeason } = useSelector(
+    (state: IRootState) => state.players,
+  );
+  const [playerData, setPlayerData] = useState<IplayerData>();
+  const { data, isLoading, error } = useSWR(
+    `/api/player?name=${playerOneName}`,
+  );
+  useEffect(() => {
+    setPlayerData(data ? data[playerOneYear][playerOneSeason] : undefined);
+    dispatch(chartActions.playerOneChart(playerData));
+  }, [data, playerData]);
+  return (
+    <>
+      {!playerData ? (
+        'loading..'
+      ) : (
+        <>
+          <Button
+            onClick={() => {
+              dispatch(
+                playersActions.playerOneSelect({
+                  isSelect: false,
+                  name: '',
+                  year: '',
+                  season: '',
+                }),
+              );
+              dispatch(chartActions.playerOneReset());
+            }}
+          >
+            <svg
+              fill="white"
+              xmlns="http://www.w3.org/2000/svg"
+              version="1.1"
+              x="0px"
+              y="0px"
+              viewBox="0 0 1000 1000"
+              enableBackground="new 0 0 1000 1000"
+            >
+              <g>
+                <path d="M864.2,559.6c0,201.3-162.9,364.2-364.2,364.2S135.8,760.9,135.8,559.6c0-201.3,162.9-364.2,364.2-364.2c25.2,0,49,2.6,72.8,7.9l-94,94l46.4,46.4L692,176.9L525.2,10l-46.4,46.4l76.8,76.8c-18.5-1.3-37.1-4-55.6-4c-237.1,0-430.4,193.4-430.4,430.4C69.6,796.6,262.9,990,500,990c237.1,0,430.4-193.4,430.4-430.4H864.2z" />
+              </g>
+            </svg>
+          </Button>
+          <PlayerCard
+            name={playerOneName}
+            year={playerOneYear}
+            season={playerOneSeason}
+            data={playerData}
+          ></PlayerCard>
+        </>
+      )}
+    </>
+  );
+};
+
+export default PlayerCard1Container;

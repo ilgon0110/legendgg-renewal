faece93d9463b2999ed81b0b5090ba621db5e890
ilgon0110 <moses011010@gmail.com>
2023-01-21 15:29:15 +0900

    feat: PlayerChart Component 구현
    player page chart를 담당하는 Component
    기존 props가 아닌 Redux를 사용하여 player page와 의존성 분리

---
+++ components/NoCompared.tsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import { media } from '../styles/theme';
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: url('${process.env.PUBLIC_URL}/imgs/cat.png');
+  background-repeat: no-repeat;
+  background-position: 50% 70%;
+  background-size: 15%;
+  border: 1px solid red;
+  ${() => media.tablet} {
+    background-position: center;
+  }
+  ${() => media.mobile} {
+    height: 400px;
+    background-position: center;
+  }
+`;
+const Loading = styled.h1`
+  margin-top: 15%;
+  margin-bottom: 16px;
+  color: white;
+  font-size: 24px;
+  font-weight: normal;
+  text-align: center;
+  ${() => media.tablet} {
+    font-size: 16px;
+  }
+`;
+const Text = styled.h2`
+  color: white;
+  font-size: 20px;
+  font-weight: normal;
+  text-align: center;
+  ${() => media.tablet} {
+    font-size: 14px;
+  }
+`;
+function NoCompared() {
+  return (
+    <Wrapper>
+      <Loading>
+        13-14시즌 선수들은 15시즌 이후 선수들과 비교가 불가능합니다.
+      </Loading>
+    </Wrapper>
+  );
+}
+
+export default NoCompared;
---
+++ components/PlayerChart.tsx
@@ -0,0 +1,169 @@
+import dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { IRootState } from '../store';
+import useSWR from 'swr';
+
+const ApexChart = dynamic(() => import('react-apexcharts'), {
+  ssr: false,
+});
+
+function PlayerChart() {
+  const {
+    chart: { playerOne, playerTwo },
+    players: { playerOneYear, playerOneSeason, playerTwoYear, playerTwoSeason },
+  } = useSelector((state: IRootState) => state);
+
+  const toFixedOne = (arr: any) => {
+    return arr.map((v: any) => {
+      return { ...v, y: v.y ? v.y.toFixed(1) : 0 };
+    });
+  };
+  const playerOneChartData2013 = toFixedOne([
+    {
+      x: 'KDA',
+      y: playerOne?.kda,
+    },
+    {
+      x: 'KP',
+      y: playerOne?.kp,
+    },
+    {
+      x: 'DTH',
+      y: playerOne?.dth,
+    },
+    {
+      x: 'GOLD',
+      y: playerOne?.gold,
+    },
+    {
+      x: 'WIN',
+      y: playerOne?.winRate,
+    },
+  ]);
+  const playerOneChartData2015 = toFixedOne([
+    {
+      x: 'KDA',
+      y: playerOne?.kda,
+    },
+    {
+      x: 'GD@10',
+      y: playerOne?.gd10,
+    },
+    {
+      x: 'CS@10',
+      y: playerOne?.cs10,
+    },
+    {
+      x: 'DPM',
+      y: playerOne?.dpm / 10,
+    },
+    {
+      x: 'DMG',
+      y: playerOne?.dmg,
+    },
+  ]);
+  const playerTwoChartData2013 = toFixedOne([
+    {
+      x: 'KDA',
+      y: playerTwo?.kda,
+    },
+    {
+      x: 'KP',
+      y: playerTwo?.kp,
+    },
+    {
+      x: 'DTH',
+      y: playerTwo?.dth,
+    },
+    {
+      x: 'GOLD',
+      y: playerTwo?.gold,
+    },
+    {
+      x: 'WIN',
+      y: playerTwo?.winRate,
+    },
+  ]);
+  const playerTwoChartData2015 = toFixedOne([
+    {
+      x: 'KDA',
+      y: playerTwo?.kda,
+    },
+    {
+      x: 'GD@10',
+      y: playerTwo?.gd10,
+    },
+    {
+      x: 'CS@10',
+      y: playerTwo?.cs10,
+    },
+    {
+      x: 'DPM',
+      y: playerTwo?.dpm / 10,
+    },
+    {
+      x: 'DMG',
+      y: playerTwo?.dmg,
+    },
+  ]);
+
+  return (
+    <ApexChart
+      type="radar"
+      width={'120%'}
+      height={'100%'}
+      series={[
+        {
+          name: `${playerOneYear} ${playerOneSeason}`,
+          data:
+            Number(playerOneYear) < 2015
+              ? playerOneChartData2013
+              : playerOneChartData2015,
+        },
+        {
+          name: `${playerTwoYear} ${playerTwoSeason}`,
+          data:
+            Number(playerTwoYear) < 2015
+              ? playerTwoChartData2013
+              : playerTwoChartData2015,
+        },
+      ]}
+      options={{
+        chart: {
+          toolbar: {
+            show: false,
+          },
+          foreColor: '#FFFFFF',
+          width: '100%',
+          height: '100%',
+          type: 'radar',
+        },
+        xaxis: {
+          categories:
+            Number(playerOneYear) < 2015
+              ? ['KDA', 'KP%', 'DTH%', 'GOLD', 'WIN%']
+              : ['KDA', 'GD@10', 'CS@10', 'DPM', 'DMG%'],
+        },
+        yaxis: {
+          show: false,
+          tickAmount: 4,
+          min: -100,
+          max: 100,
+        },
+        dataLabels: {
+          enabled: true,
+          formatter: (element: number, { dataPointIndex }) =>
+            Number(playerOneYear) >= 2015 && dataPointIndex === 3
+              ? element * 10
+              : element,
+        },
+        tooltip: {
+          enabled: false,
+        },
+      }}
+    ></ApexChart>
+  );
+}
+
+export default PlayerChart;

fcb971577a3a91fdabcbfa264f160e8fedf9bcf1
ilgon0110 <moses011010@gmail.com>
2023-02-07 20:35:35 +0900

    feat: _app.js 수정
    useSWR fetcher 전역으로 설정
    SessionProvider 설정
    Redux Provider 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,39 @@
 import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from '../styles/global-style';
+import { GlobalStyle } from '@styles/global-style';
 import type { AppProps } from 'next/app';
 import { theme } from '../styles/theme';
 import Head from 'next/head';
-import Layout from '../components/Layout';
+import Layout from '@components/Layout';
+import { Provider } from 'react-redux';
+import { store } from '@store/index';
+import { SWRConfig } from 'swr';
+import { SessionProvider } from 'next-auth/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const fetcher = (url: string) =>
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => json)
+      .then((json) => JSON.parse(json));
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>boilerplate</title>
+        <title></title>
       </Head>
       <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <SWRConfig value={{ fetcher: fetcher }}>
+            <SessionProvider session={pageProps.session}>
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            </SessionProvider>
+          </SWRConfig>
+        </ThemeProvider>
+      </Provider>
     </>
   );
 }

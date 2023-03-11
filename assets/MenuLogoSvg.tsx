7987e67ec32eacfb16359b1cf44ca5f86f516336
ilgon0110 <moses011010@gmail.com>
2023-03-11 18:58:16 +0900

    refactor: assets component 생성
    자주 쓰이는 SVG들을 asset으로 관리

---
+++ assets/MenuLogoSvg.tsx
@@ -0,0 +1,18 @@
+const MenuLogoSvg = () => {
+  return (
+    <>
+      <path
+        d="M0 16C0 7.16344 7.16344 0 16 0H37C45.8366 0 53 7.16344 53 16V32H16C7.16344 32 0 24.8366 0 16Z"
+        fill="#ffffff"
+        stopColor="#ffffff"
+      />
+      <rect x="6" y="6" width="41" height="20" rx="10" fill="#bbbbbb" stopColor="#bbbbbb" />
+      <circle cx="16" cy="16" r="5" fill="#000000" />
+      <circle cx="14" cy="14" r="1" fill="#ffffff" />
+      <circle cx="38" cy="16" r="5" fill="#000000" />
+      <circle cx="36" cy="14" r="1" fill="#ffffff" />
+    </>
+  );
+};
+
+export default MenuLogoSvg;
